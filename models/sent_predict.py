ca02e52cd740d94eefd7b629cb2c7105662a5523
Abhishek Prakash Singh <abhishek.prakash078@gmail.com>
2017-12-28 12:06:33 +0530

    Minor changes for scalability to multiple movies

--- models/sent_predict.py
+++ models/sent_predict.py
@@ -24,6 +24,8 @@ from tweepy import API
 import pymongo
 import datetime
 
+movie_list = ['TigerZindaHai','SonuKeTituKiSweety','Padman','1921','ThaanaaSerndhaKoottam','FukreyReturns','Mukkabaaz','MonsoonShootout']
+
 #Variables that contains the user credentials to access Twitter API 
 access_token = ""
 access_token_secret = ""
@@ -34,14 +36,14 @@ tweets_data = []
 
 # Preprocess the extracted tweets to be predicted by the model
 def preprocess(tweets_data):
-    count_vectorizer = joblib.load('../data/vectorizer.pkl')		# when running with crontab use absolute path
+    count_vectorizer = joblib.load('/home/abhishek/Trend-Analyzer/data/vectorizer.pkl')		# when running with crontab use absolute path
     tweets_data = count_vectorizer.transform(tweets_data)
     tfidf_tweet = TfidfTransformer(use_idf=False).transform(tweets_data)		# Tf-idf matrix for tweets
     return tfidf_tweet
 
 # learning the naive bayes classifier to classify the data
 def predict_sentiment(tweets_data):
-    classifier = joblib.load('../data/Naive_Bayes.pkl')			# when running with crontab use absolute path
+    classifier = joblib.load('/home/abhishek/Trend-Analyzer/data/Naive_Bayes.pkl')			# when running with crontab use absolute path
     predicted_tweets = classifier.predict(tweets_data)
     return predicted_tweets
 
@@ -58,51 +60,54 @@ def main():
 	auth = OAuthHandler(consumer_key, consumer_secret)
 	auth.set_access_token(access_token, access_token_secret)
 	api = API(auth)
-	tweets_list = api.search(q='TigerZindaHai', count=100)				# retrieve top 100 tweets for the movie
-	for tweet in tweets_list:
-		tweets_data.append(tweet.text)
+	for item in movie_list:
+		tweets_data = list()
+		tweets_list = api.search(q=item, count=100)				# retrieve top 100 tweets for the movie
+		for tweet in tweets_list:
+			tweets_data.append(tweet.text)
 
-	# Preprocessing tweets to give as input to the classifier
-	tfidf_tweet = preprocess(tweets_data)
+		# Preprocessing tweets to give as input to the classifier
+		tfidf_tweet = preprocess(tweets_data)
 
-	# Predicted output received from the classifier
-	predicted_output = predict_sentiment(tfidf_tweet)
+		# Predicted output received from the classifier
+		predicted_output = predict_sentiment(tfidf_tweet)
 
-	pos_count = 0
-	neg_count = 0
+		pos_count = 0
+		neg_count = 0
 
-	# Checking whether the tweet has a positive or negative sentiment
-	for i in range(len(tweets_data)):
-		if predicted_output[i]=='1':
-			sent="Positive"
-			pos_count=pos_count+1
-		else:
-			sent="Negative"
-			neg_count=neg_count+1
+		# Checking whether the tweet has a positive or negative sentiment
+		for i in range(len(tweets_data)):
+			if predicted_output[i]=='1':
+				sent="Positive"
+				pos_count=pos_count+1
+			else:
+				sent="Negative"
+				neg_count=neg_count+1
+			
+		# Displaying statistics
+		print("Total tweets: %d") %(i+1)
+		print("Positive tweets: %d") %(pos_count)
+		print("Negative tweets: %d") %(neg_count)
+		perc_pos = float(pos_count)/(i+1)*100
+		print("Percentage of positive tweets: %f%%") %(perc_pos)
+
+		time_str,time_list = time()
 		
-	# Displaying statistics
-	print("Total tweets: %d") %(i+1)
-	print("Positive tweets: %d") %(pos_count)
-	print("Negative tweets: %d") %(neg_count)
-	perc_pos = float(pos_count)/(i+1)*100
-	print("Percentage of positive tweets: %f%%") %(perc_pos)
+		# Creating appropriate data structure to store in mongodb
+		dic = {}
+		dic["name"] = item
+		dic["perc"] = perc_pos
+		dic["date"] = time_list[0]
+		dic["time"] = time_list[1]
+		print(dic)
+		pos_dict = {}
+		pos_dict[time_str] = dic
+		print(pos_dict)
 
-	time_str,time_list = time()
-	
-	# Creating appropriate data structure to store in mongodb
-	dic = {}
-	dic["perc"] = perc_pos
-	dic["date"] = time_list[0]
-	dic["time"] = time_list[1]
-	print(dic)
-	pos_dict = {}
-	pos_dict[time_str] = dic
-	print(pos_dict)
-
-	# Storing this information in mongodb for time series visualization
-	client = pymongo.MongoClient()
-	db = client.tweet_sent
-	db.twitter_data.insert(pos_dict,check_keys=False)
+		# Storing this information in mongodb for time series visualization
+		client = pymongo.MongoClient()
+		db = client.tweet_sent
+		db.twitter_data.insert(pos_dict,check_keys=False)
 	
 if __name__ == "__main__":
 	main()
